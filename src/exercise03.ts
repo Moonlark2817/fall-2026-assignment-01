d3bd633b79e7d24505694602cdc849f95c3c702f
RagnyaSri Uriti <ragnyasriuriti@RagnyaSris-MacBook-Air.local>
2026-09-02 11:35:30 -0400

    Complete exercise 3 inventory metrics

--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,18 @@
+// export function getInventoryValue(
+//   inventory: Array<[string, number, number]>,
+// ): number {
+//   return 0;
+// }
+
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
+  const eligibleItems = inventory.filter((item) => item[1] > 5);
+
+  const totalValue = eligibleItems.reduce(
+    (total, item) => total + item[1] * item[2],
+    0,
+  );
+
+  return totalValue;
 }
