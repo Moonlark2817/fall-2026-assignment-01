d115eb7569f111c049e78392f33b3ae9e5f2cd02
RagnyaSri Uriti <ragnyasriuriti@RagnyaSris-MacBook-Air.local>
2026-09-02 11:00:57 -0400

    Complete exercise 1 name formatter

--- src/exercise01.ts
+++ src/exercise01.ts
@@ -1,7 +1,21 @@
+//   export function formatName(
+//   firstName: string,
+//   lastName: string,
+//   middleName?: string | null,
+// ): string {
+//   return '';
+// }
+
 export function formatName(
   firstName: string,
   lastName: string,
   middleName?: string | null,
 ): string {
-  return '';
+  let formattedName = lastName + ', ' + firstName;
+
+  if (middleName) {
+    formattedName += ' ' + middleName[0] + '.';
+  }
+
+  return formattedName;
 }
