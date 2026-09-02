c046c4529a31cc470d27e8ee549200f32ab32d65
RagnyaSri Uriti <ragnyasriuriti@RagnyaSris-MacBook-Air.local>
2026-09-02 12:44:42 -0400

    Complete exercise 8 event emitter

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,24 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private handlers: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    const eventHandlers = this.handlers[eventName] ?? [];
+    eventHandlers.push(callback);
+    this.handlers[eventName] = eventHandlers;
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const eventHandlers = this.handlers[eventName] ?? [];
+
+    eventHandlers.forEach((callback) => {
+      callback(data);
+    });
+  }
 }
