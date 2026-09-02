57f662a3ae4a074a9990b89169cd54c04dfc40a3
RagnyaSri Uriti <ragnyasriuriti@RagnyaSris-MacBook-Air.local>
2026-09-02 11:24:31 -0400

    Complete exercise 2 DNA transcription

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,30 @@
+// export function transcribeDNA(dna: string): string {
+//   return '';
+// }
+
+export class InvalidDNAError extends Error {
+  constructor(nucleotide: string) {
+    super('Invalid DNA nucleotide: ' + nucleotide);
+    this.name = 'InvalidDNAError';
+  }
+}
+
 export function transcribeDNA(dna: string): string {
-  return '';
+  let rna = '';
+
+  for (const nucleotide of dna) {
+    if (nucleotide === 'A') {
+      rna += 'U';
+    } else if (nucleotide === 'T') {
+      rna += 'A';
+    } else if (nucleotide === 'C') {
+      rna += 'G';
+    } else if (nucleotide === 'G') {
+      rna += 'C';
+    } else {
+      throw new InvalidDNAError(nucleotide);
+    }
+  }
+
+  return rna;
 }
